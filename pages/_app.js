d16f0410434e5d7298a550a249ef0731c4335602
Sagar Dalal <sagardalal112@gmail.com>
2021-07-25 18:13:06 +0530

    Github Link added

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import { Provider } from "next-auth/client";
 
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import GithubLink from "../components/github-link";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
       <Layout>
         <Component {...pageProps} />
+        <GithubLink />
       </Layout>
     </Provider>
   );
